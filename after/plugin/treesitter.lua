bdf229fc40631da471cfb9e2cd5462fe9321bce4
y-syo <mmoussou@student.42angouleme.fr>
2024-04-22 01:20:07 +0200

    [🏗️] wip(treesitter.nvim): added default python and markdown

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -6,12 +6,12 @@
 --   By: mmoussou <mmoussou@student.42angouleme.fr  +#+  +:+       +#+        --
 --                                                +#+#+#+#+#+   +#+           --
 --   Created: 2024/01/18 07:12:38 by mmoussou          #+#    #+#             --
---   Updated: 2024/01/18 07:22:19 by mmoussou         ###   ########.fr       --
+--   Updated: 2024/04/02 23:26:55 by mmoussou         ###   ########.fr       --
 --                                                                            --
 -- ************************************************************************** --
 
 require("nvim-treesitter.configs").setup({
-	ensure_installed = { "c", "cpp", "lua", "rust" },
+	ensure_installed = { "c", "cpp", "lua", "rust", "python", "markdown" },
 	sync_install = false,
 	highlight = { enable = true },
 	indent = { enable = true },
